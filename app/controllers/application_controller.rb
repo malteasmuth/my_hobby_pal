60b0a341e8f34d781a94fe24fff1daf54a69f160
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-09-26 12:49:01 +0200

    Add username to users, display username on dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # white list approach: protect every route by default
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
