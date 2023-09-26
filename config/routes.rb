64c5b381b0be837962072920350fb84c92346ea9
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-09-26 11:44:29 +0200

    integrate devise basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/view'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#view'
 end
