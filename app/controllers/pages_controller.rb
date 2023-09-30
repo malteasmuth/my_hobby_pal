af475aa0926d7885bed334f22d17e90fa92bd414
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-09-30 16:12:06 +0200

    Find a pal method working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,33 @@ class PagesController < ApplicationController
 
   def dashboard
     @user_profile = Profil.find_by(user_id: current_user.id)
+    find_pal
+  end
+
+  def find_pal
+    # Service-Klasse, die zur Aufgabe hat, einen zufälligen User aus dem Wohnort
+    # des angemeldeten Users zu ermitteln,
+    # dessen Interessen sich mit dem eigenen Nutzer überschneiden.
+
+    @user_profile = Profil.find_by(user_id: current_user.id)
+    all_interests = @user_profile.interests
+
+    all_interests.each do |interest|
+      # all interest entries that do NOT belongt @user_profiles
+      interests = Interest.where(name: interest.name)
+                          .where.not(profil_id: @user_profile.id)
+      # all unique profiles
+      profiles = interests.collect{ |i| i.profil}.uniq
+      # all profiles at the samle place
+      profiles_place = profiles.select { |p| p.wohnort == @user_profile.wohnort}
+
+      if profiles_place.empty?
+        pal = profiles.uniq.sample
+      elsif profiles.empty?
+        nil
+      else
+        pal = profiles_place.sample
+      end
+    end
   end
 end
