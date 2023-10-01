56c478726ad6d4ededd5cf411d14bd0e573c6db4
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-10-01 13:19:10 +0200

    Implement button and display of pal name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,41 +6,12 @@ class PagesController < ApplicationController
 
   def dashboard
     @user_profile = Profil.find_by(user_id: current_user.id)
-    find_pal
+    @pal = Profil.find(params[:pal]) unless params[:pal].nil?
   end
 
-  def find_pal
-    # Service-Klasse, die zur Aufgabe hat, einen zufälligen User aus dem Wohnort
-    # des angemeldeten Users zu ermitteln,
-    # dessen Interessen sich mit dem eigenen Nutzer überschneiden.
-
+  def pal_finder
     @user_profile = Profil.find_by(user_id: current_user.id)
-    all_interests = @user_profile.interests
-    possible_pals = []
-
-    all_interests.each do |interest|
-      # all interest entries that do NOT belong to @user_profiles
-      interests = Interest.where(name: interest.name)
-                          .where.not(profil_id: @user_profile.id)
-      # all unique profiles
-      profiles = interests.collect{ |i| i.profil}.uniq
-      # all profiles at the samle place
-
-      profiles.each do |profil|
-        possible_pals << profil
-      end
-    end
-
-
-    same_place_profiles = possible_pals.select { |p| p.wohnort == @user_profile.wohnort}
-
-    if same_place_profiles.empty? && possible_pals.empty?
-      nil
-    elsif same_place_profiles.empty?
-      pal = possible_pals.sample
-    else
-      pal = same_place_profiles.sample
-    end
-    raise
+    @pal = PalFinder.new(@user_profile).find_pal
+    redirect_to pages_dashboard_path(pal: @pal)
   end
 end
---
+++ app/services/pal_finder.rb
@@ -0,0 +1,35 @@
+class PalFinder
+  def initialize(profile)
+    @current_profile = profile
+  end
+
+  def find_pal
+    # Gibt einen zufälligen User aus dem Wohnort
+    # des angemeldeten Users zurück,
+    # dessen Interessen sich mit dem eigenen Nutzer überschneiden.
+
+    all_interests = @current_profile.interests
+    possible_pals = []
+
+    all_interests.each do |interest|
+      # all interest entries that do NOT belong to @user_profiles
+      interests = Interest.where(name: interest.name)
+                          .where.not(profil_id: @current_profile.id)
+      # all unique profiles
+      profiles = interests.collect{ |i| i.profil}.uniq
+      profiles.each do |profil|
+        possible_pals << profil
+      end
+    end
+
+    same_place_profiles = possible_pals.select { |p| p.wohnort == @current_profile.wohnort}
+
+    if same_place_profiles.empty? && possible_pals.empty?
+      nil
+    elsif same_place_profiles.empty?
+      pal = possible_pals.sample
+    else
+      pal = same_place_profiles.sample
+    end
+  end
+end
